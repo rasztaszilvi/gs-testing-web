c3b8bf99e66208d6cf04cf85198b6f999ac916d0
Szilvia Orban <rasztaszilvi@gmail.com>
2018-11-05 20:38:32 +0100

    Basic Spring boot web server "Hello Worls" and Unit tests

--- initial/src/test/java/hello/WebMockTest.java
+++ initial/src/test/java/hello/WebMockTest.java
@@ -1,6 +1,7 @@
 package hello;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -10,21 +11,24 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(HomeController.class)
-//tag::test[]
-public class WebLayerTest {
+@WebMvcTest(GreetingController.class)
+public class WebMockTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private GreetingService service;
+
     @Test
-    public void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello World")));
+    public void greetingShouldReturnMessageFromService() throws Exception {
+        when(service.greet()).thenReturn("Hello Mock");
+        this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello Mock")));
     }
 }
-//end::test[]
